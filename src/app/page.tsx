9289d087d42d9b32e939f26648540e9b417a7549
marshadkhn <marshadkhn89@gmail.com>
2025-10-13 13:19:19 +0530

    added

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ResizableNavbar } from "@/components/ui/resizable-navbar";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <ResizableNavbar />
+    </main>
+  );
+}
